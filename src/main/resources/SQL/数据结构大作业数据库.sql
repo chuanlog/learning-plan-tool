27bf7e8988961eda5290b97046ea6e4cd9a15856
cloog <395773508@qq.com>
2025-04-21 13:02:34 +0800

    添加数据库建表文件

---
+++ src/main/resources/SQL/数据结构大作业数据库.sql
@@ -0,0 +1,39 @@
+CREATE TABLE course
+(
+    id          bigint AUTO_INCREMENT COMMENT '课程ID（主键）'
+        PRIMARY KEY,
+    course_name varchar(255) NOT NULL COMMENT '课程名称',
+    credits     int          NOT NULL COMMENT '学分',
+    total_hours int          NOT NULL COMMENT '总课时',
+    course_type int          NOT NULL COMMENT '课程类型（如选修、必修等）',
+    user_id     bigint       NOT NULL COMMENT '所属用户ID（需应用层保证关联性）'
+)
+    COMMENT '课程信息表' COLLATE = utf8mb4_general_ci;
+
+CREATE INDEX idx_user_id
+    ON course (user_id);
+
+CREATE TABLE course_prerequisite
+(
+    course_id       int NOT NULL COMMENT '当前课程ID',
+    prerequisite_id int NOT NULL COMMENT '先修课程ID',
+    PRIMARY KEY (course_id, prerequisite_id)
+);
+
+CREATE INDEX idx_course_id
+    ON course_prerequisite (course_id);
+
+CREATE INDEX idx_prerequisite_id
+    ON course_prerequisite (prerequisite_id);
+
+CREATE TABLE user
+(
+    id       bigint AUTO_INCREMENT COMMENT '主键'
+        PRIMARY KEY,
+    username varchar(255) COLLATE utf8mb3_bin NOT NULL COMMENT '用户名',
+    password varchar(255) COLLATE utf8mb3_bin NOT NULL COMMENT '密码',
+    CONSTRAINT username
+        UNIQUE (username)
+);
+INSERT INTO user (username, password)
+VALUES ('admin', 'e10adc3949ba59abbe56e057f20f883e');
