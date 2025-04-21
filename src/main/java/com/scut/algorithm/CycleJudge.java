769a15e6194b532543f2b3a5acf286e0d8d32e60
cloog <395773508@qq.com>
2025-04-21 16:08:25 +0800

    添加了判圈算法的接口,等待实现

--- src/main/java/com/scut/algorithm/CycleJudge.java
+++ src/main/java/com/scut/algorithm/CycleJudge.java
@@ -3,11 +3,11 @@ package com.scut.algorithm;
 import com.scut.entity.CoursePrerequisite;
 
 import java.util.List;
-//TODO 实现判圈算法：交给队友搞
+//TODO 实现判圈算法接口--交给队友搞
 public interface CycleJudge {
     /**
      * 判断当前课程依赖是否有环，用来在添加课程时动态判断是否有环
-     * @param relations 关系列表
+     * @param relations 关系列表,其中CoursePrerequisite类的courseId为当前课程id，prerequisiteId为当前课程的先修课程id
      * @param courseIds 课程id列表
      * @return
      */
