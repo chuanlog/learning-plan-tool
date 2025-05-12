ae0d4579fd278da02f9b7d821aa19f573aefc853
cloog <395773508@qq.com>
2025-05-13 00:18:30 +0800

    配置跨域问题

--- src/main/java/com/scut/config/InterceptorConfig.java
+++ src/main/java/com/scut/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import com.scut.interceptor.JwtTokenUserInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,16 @@ public class InterceptorConfig implements WebMvcConfigurer {
     
     @Autowired
     private JwtTokenUserInterceptor jwtTokenUserInterceptor;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("跨域拦截器加载...");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
     
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
